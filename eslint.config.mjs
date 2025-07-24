237363b4f1cf2dad497202bf07880e2c5b5d7eea
Ladislao García <malcomcc@gmail.com>
2025-07-24 03:17:06 +0200

    feat: 🎉 Dependencies configuration files

---
+++ eslint.config.mjs
@@ -0,0 +1,78 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import globals from 'globals';
+import js from '@eslint/js';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
+import tsparser from '@typescript-eslint/parser';
+
+export default defineConfig([
+  globalIgnores(['./package.json', './package-lock.json', 'dist/']),
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    languageOptions: {
+      parser: tsparser,
+      sourceType: 'module',
+    },
+
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...prettierConfig.rules,
+      ...tseslint.configs.recommended.rules,
+      'no-console': 'warn',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      ...js.recommended.rules,
+      '@typescript-eslint/no-require-imports': ['off']
+    }
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
+]);
