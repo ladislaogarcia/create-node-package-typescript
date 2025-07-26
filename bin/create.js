8bd734a2909629e4dfdbbcf67dc4ea051cbac397
Ladislao García <malcomcc@gmail.com>
2025-07-26 19:52:18 +0200

    refactor: 🔧 Remove folder creation + Extra code

--- bin/create.js
+++ bin/create.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 'use strict';
 
-const { cpSync, mkdirSync, existsSync } = require('node:fs');
+const { cpSync } = require('node:fs');
 const { sep, join } = require('node:path');
 const fullpath = process.argv[1];
+console.log('fullpath', fullpath);
 const src = fullpath.split('bin')[0];
 const folderName = src
   .split(sep)
@@ -12,19 +13,6 @@ const folderName = src
 const dest = join(fullpath.split('node_modules')[0], folderName);
 
 const createItems = () => {
-  if (existsSync(dest)) {
-    console.log(`Folder "${folderName}" already exists.`);
-    console.log(`You must renove it or use another one.`);
-    return;
-  }
-  try {
-    mkdirSync(dest, { recursive: true });
-    console.log(`Folder ${folderName} has been created`);
-  } catch (error) {
-    console.log(`Error creating folder ${folderName}: ${error.message}`);
-    return;
-  }
-
   try {
     cpSync(src, dest, { recursive: true });
     console.log(`Files are created`);
@@ -32,10 +20,6 @@ const createItems = () => {
     console.error(`Error copying files: ${error.message}`);
   }
 };
-
-// const src = dirname();
-// const dest = process.cwd();
-// cpSync(src, dest, {recursive: true});
 
 (() => {
   createItems();
